dba38a7524b4a3b18fd76ce4fd16133eee63648d
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-13 12:53:19 -0400

    removed map

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnStartQuiz = document.querySelector('.startQuiz');
 const removeStartBtn = document.querySelector('.startBtn');
-let displayQuestionOne = document.querySelector('#question');
+let displayQuestionData = document.querySelector('#question');
 
 let displayAnswerOne = document.querySelector('#answer1');
 let displayAnswerTwo = document.querySelector('#answer2');
@@ -11,7 +11,7 @@ const btnNext = document.querySelector('.next');
 const btnSubmit = document.querySelector('.submit');
 
 let sliderNumber = 0;
-let questionNumber = 0;
+
 let points = 0;
 
 const quizQuestions = [
@@ -30,11 +30,10 @@ btnStartQuiz.addEventListener('click', (e) => {
     removeStartBtn.style.display = 'none';
     showButtons();
     showQuestion();
-    showNextQuestion();
+    displayAnswerOptions(0);
 
     btnNext.addEventListener('click', showNextQuestion);
-});
-
+})
 
 function showButtons() {
     const btnNextShow = document.querySelector('.buttons');
@@ -52,18 +51,16 @@ function showQuestion() {
 
 
 
-function showNextQuestion() {
-    // e.preventDefault();
-
-    // let questionOne = quizQuestions.map(question => question[0]);
-    // displayQuestionOne.textContent = questionOne[0];
-
-
-    displayAnswerOptions(sliderNumber, questionNumber);
+function showNextQuestion(e) {
+    e.preventDefault();
     sliderNumber++;
-    questionNumber++;
+    displayAnswerOptions(sliderNumber);
 
-
+    if (sliderNumber === 4) {
+        btnNext.style.display = 'none';
+        btnSubmit.style.display = 'block';
+        btnSubmit.addEventListener('click', showResults);
+    }
 }
 
 function showResults(e) {
@@ -74,136 +71,12 @@ function showResults(e) {
 }
 
 
-function displayAnswerOptions(sliderNumber, questionNumber) {
+function displayAnswerOptions(sliderNumber) {
+    displayQuestionData.textContent = quizQuestions[sliderNumber][0]
 
-    let questionOne = quizQuestions.map(question => question[questionNumber]);
-    displayQuestionOne.textContent = questionOne[questionNumber];
-
-    let answerOne = quizQuestions.map(answer => answer[1]);
-    displayAnswerOne.textContent = answerOne[sliderNumber][0];
-    displayAnswerTwo.textContent = answerOne[sliderNumber][1];
-    displayAnswerThree.textContent = answerOne[sliderNumber][2];
-    displayAnswerFour.textContent = answerOne[sliderNumber][3];
-
-}
-
-
-
-
-
-
-
-
-
-// function showQuestionTwo(e) {
-//     e.preventDefault();
-
-//     let questionOne = quizQuestions.map(question => question[0]);
-//     displayQuestionOne.textContent = questionOne[1];
-
-
-//     // let answerOne = quizQuestions.map(answer => answer[1]);
-//     displayAnswerOptions(sliderNumber);
-//     sliderNumber++;
-
-//     // displayAnswerOne.textContent = answerOne[1][0];
-//     // displayAnswerTwo.textContent = answerOne[1][1];
-//     // displayAnswerThree.textContent = answerOne[1][2];
-//     // displayAnswerFour.textContent = answerOne[1][3];
-//     if (displayAnswerTwo.checked) {
-//         points++;
-//     }
-
-//     btnNext.addEventListener('click', showQuestionThree);
-
-// }
-
-
-// function showQuestionThree(e) {
-//     e.preventDefault();
-
-//     let questionOne = quizQuestions.map(question => question[0]);
-//     displayQuestionOne.textContent = questionOne[2];
-//     // let answerOne = quizQuestions.map(answer => answer[1]);
-
-//     displayAnswerOptions(sliderNumber);
-//     sliderNumber++;
-//     console.log(displayAnswerOptions);
-//     // displayAnswerOne.textContent = answerOne[2][0];
-//     // displayAnswerTwo.textContent = answerOne[2][1];
-//     // displayAnswerThree.textContent = answerOne[2][2];
-//     // displayAnswerFour.textContent = answerOne[2][3];
-//     if (displayAnswerThree.checked) {
-//         points++;
-//     }
-//     btnNext.addEventListener('click', showQuestionFour)
-// }
-
-// function showQuestionFour(e) {
-//     e.preventDefault();
-
-//     let questionOne = quizQuestions.map(question => question[0]);
-//     displayQuestionOne.textContent = questionOne[3];
-
-
-//     // let answerOne = quizQuestions.map(answer => answer[1]);
-
-//     displayAnswerOptions(sliderNumber);
-//     sliderNumber++;
-
-//     // displayAnswerOne.textContent = answerOne[3][0];
-//     // displayAnswerTwo.textContent = answerOne[3][1];
-//     // displayAnswerThree.textContent = answerOne[3][2];
-//     // displayAnswerFour.textContent = answerOne[3][3];
-//     if (displayAnswerFour.checked) {
-//         points++;
-//     }
-//     btnNext.addEventListener('click', showQuestionFive)
-// }
-
-
-// function showQuestionFive(e) {
-//     e.preventDefault();
-
-//     let questionOne = quizQuestions.map(question => question[0]);
-//     displayQuestionOne.textContent = questionOne[4];
-
-
-//     // let answerOne = quizQuestions.map(answer => answer[1]);
-
-//     displayAnswerOptions(sliderNumber);
-//     sliderNumber++;
-
-//     // displayAnswerOne.textContent = answerOne[4][0];
-//     // displayAnswerTwo.textContent = answerOne[4][1];
-//     // displayAnswerThree.textContent = answerOne[4][2];
-//     // displayAnswerFour.textContent = answerOne[4][3];
-//     if (displayAnswerOne.checked) {
-//         points++;
-//     }
-//     btnNext.style.display = 'none';
-//     btnSubmit.style.display = 'block';
-//     btnSubmit.addEventListener('click', showResults);
-// }
-
-
-function showResults(e) {
-    e.preventDefault();
-    const par = document.querySelector('#show');
-    par.style.display = 'block';
-    par.textContent = "Your points: " + points;
-}
-
-
-function displayAnswerOptions(sliderNumber, questionNumber) {
-
-    let questionOne = quizQuestions.map(question => question[questionNumber]);
-    displayQuestionOne.textContent = questionOne[questionNumber];
-
-    let answerOne = quizQuestions.map(answer => answer[1]);
-    displayAnswerOne.textContent = answerOne[sliderNumber][0];
-    displayAnswerTwo.textContent = answerOne[sliderNumber][1];
-    displayAnswerThree.textContent = answerOne[sliderNumber][2];
-    displayAnswerFour.textContent = answerOne[sliderNumber][3];
+    displayAnswerOne.textContent = quizQuestions[sliderNumber][1][0];
+    displayAnswerTwo.textContent = quizQuestions[sliderNumber][1][1];
+    displayAnswerThree.textContent = quizQuestions[sliderNumber][1][2];
+    displayAnswerFour.textContent = quizQuestions[sliderNumber][1][3];
 
 }
