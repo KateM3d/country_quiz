d01526322050a2c5fd9064efb27fe021ead96dcb
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-13 20:12:05 -0400

    added final calculation

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const btnStartQuiz = document.querySelector('.startQuiz');
-const removeStartBtn = document.querySelector('.startBtn');
-let displayQuestionData = document.querySelector('#question');
 
-let displayAnswerOne = document.querySelector('#answer1');
-let displayAnswerTwo = document.querySelector('#answer2');
-let displayAnswerThree = document.querySelector('#answer3');
-let displayAnswerFour = document.querySelector('#answer4');
 
 const btnNext = document.querySelector('.next');
 const btnSubmit = document.querySelector('.submit');
 
 let sliderNumber = 0;
+let lastSlider = 4;
 
 let points = 0;
+
+
+
 
 const quizQuestions = [
     ["The body of the Egyptian Sphinx was based on which animal?", ["Lion", "Fish", "Bird", "Human"], 0],
@@ -26,15 +24,18 @@ const quizQuestions = [
 
 btnStartQuiz.addEventListener('click', (e) => {
     e.preventDefault();
-
+    const removeStartBtn = document.querySelector('.startBtn');
     removeStartBtn.style.display = 'none';
     showButtons();
     showQuestion();
-    displayAnswerOptions(0);
+    displayAnswerOptions(sliderNumber);
 
     btnNext.addEventListener('click', showNextQuestion);
 
 })
+
+
+
 
 function showButtons() {
     const btnNextShow = document.querySelector('.buttons');
@@ -50,22 +51,36 @@ function showQuestion() {
 
 }
 
+function checkAnswer() {
+    let correctAnswerIndex = quizQuestions[sliderNumber][2];
+    if (selectedAnswer[correctAnswerIndex].checked) {
+        points++;
+    }
 
+    console.log(selectedAnswer[correctAnswerIndex].checked);
+}
+
+let selectedAnswer = document.querySelectorAll('input');
 
 function showNextQuestion(e) {
     e.preventDefault();
+
+    checkAnswer();
+
     sliderNumber++;
     displayAnswerOptions(sliderNumber);
 
-    if (sliderNumber === 4) {
+    if (sliderNumber === lastSlider) {
         btnNext.style.display = 'none';
         btnSubmit.style.display = 'block';
         btnSubmit.addEventListener('click', showResults);
     }
 }
 
+
 function showResults(e) {
     e.preventDefault();
+    checkAnswer();
     const par = document.querySelector('#show');
     par.style.display = 'block';
     par.textContent = "Your points: " + points;
@@ -73,6 +88,14 @@ function showResults(e) {
 
 
 function displayAnswerOptions(sliderNumber) {
+
+    let displayQuestionData = document.querySelector('#question');
+
+    let displayAnswerOne = document.querySelector('#answer1');
+    let displayAnswerTwo = document.querySelector('#answer2');
+    let displayAnswerThree = document.querySelector('#answer3');
+    let displayAnswerFour = document.querySelector('#answer4');
+
     displayQuestionData.textContent = quizQuestions[sliderNumber][0]
 
     displayAnswerOne.textContent = quizQuestions[sliderNumber][1][0];
@@ -80,6 +103,13 @@ function displayAnswerOptions(sliderNumber) {
     displayAnswerThree.textContent = quizQuestions[sliderNumber][1][2];
     displayAnswerFour.textContent = quizQuestions[sliderNumber][1][3];
 
-}
+    // selectedAnswer.forEach(answer => {
+    //     if (answer.checked && (Number(answer.value) === Number(correctAnswerIndex))) {
 
-let getCorrectAnswer = quizQuestions[sliderNumber][2];
+    //         points++;
+
+    //     }
+
+    // })
+
+}
